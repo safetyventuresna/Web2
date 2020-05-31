bddf9fcbb3655aa8e763e21d65b630e1b2d40a46
Joydip Saha <joydipsaha@Joydips-MacBook-Pro.local>
2020-05-31 16:12:10 -0400

    Added indentation

--- Webpage/views.py
+++ Webpage/views.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
-my_dict = {'insert_me':"ToKnock"}
-return render(request,'index.html',context=my_dict)
+    my_dict = {'insert_me':"ToKnock"}
+    return render(request,'index.html',context=my_dict)
