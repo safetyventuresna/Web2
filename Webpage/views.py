49bd8bcfb54720f69ed33eb58b655d7fe395554e
Joydip Saha <joydipsaha@Joydips-MacBook-Pro.local>
2020-05-31 15:56:19 -0400

    Added function header

--- Webpage/views.py
+++ Webpage/views.py
@@ -4,5 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
 my_dict = {'insert_me':"ToKnock"}
 return render(request,'index.html',context=my_dict)
